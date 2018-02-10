2262e2aed7f80f708edc42250b03e6a61d0df4a0
yaypm <aliemslie@gmail.com>
2018-02-10 02:34:40 +0000

    Now retrieves details

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -336,61 +336,6 @@ module.exports = function(app, db) {
 	});	
 
 	app.post('/updateAssessment', (req, res) => {	
-		var company = req.body.company;
-		var business = req.body.business;
-		var operations = req.body.operations;
-		var development = req.body.development;
-		var id = generateId();		
-		
-		var assessment = {
-			_id: id,
-			company: company,
-			business: business,
-			operations: operations,
-			development: development			
-		}
-		
-		var user_assessments = {
-			_id: id,
-			company: company,
-			username: 'alistair.emslie@dynatrace.com'
-		}
-		
-		var assessment_data = {
-			_id: id,
-			company_revenue: '',
-			projected_growth: '',
-			revenue_dependent: '',
-			app_uptime: '',
-			revenue_breach: '',
-			incidents_month: '',
-			no_ops_troubleshoot: '',
-			no_dev_troubleshoot: '',
-			mttr: '',
-			no_apps_e2e: '',
-			no_t1t2_apps: '',
-			no_fte_existing: '',
-			existing_apps: [],
-			cycles_per_year: '',
-			cycle_days: '',
-			test_per_cycle: '',
-			qa_time_per_cycle: '',
-			qa_people_per_cycle: '',
-			dev_time_per_cycle: '',
-			dev_people_per_cycle: '',
-			operation_cost: '55000',
-			developer_cost: '56000',
-			qa_cost: '46000',
-			work_hours: '1950',
-			benefit_incident_reduction: '30',
-			benefit_mttr: '90',
-			benefit_performance: '25',
-			benefit_alert_storm: '88',
-			benefit_sla: '75',
-			benefit_fix_qa: '90',
-			benefit_prod_reduction: '55',
-			benefit_config: '97'
-		}
 		
 		MongoClient.connect(connectionOptions, function(err, db) {
 			if(err) { 
@@ -401,19 +346,36 @@ module.exports = function(app, db) {
 			}
 
 			else {
-				var collection = db.collection('assessments');
-				collection.insert(assessment, {w:1}, function(err, result) { if(err!=null){console.log(err);}  console.log("added in assessment");  });
-				var collection = db.collection('user_assessments');
-				collection.insert(user_assessments, {w:1}, function(err, result) { if(err!=null){console.log(err);}  console.log("added in user_assessments");      });
 				var collection = db.collection('assessment_data');
-				collection.insert(assessment_data, {w:1}, function(err, result) { if(err!=null){console.log(err);}  console.log("added in assessment_data");      });
+				collection.update({'_id':req.header('bva_id')},{$set:{'company_revenue':req.body.company_revenue,'projected_growth':req.body.projected_growth,'revenue_dependent':req.body.revenue_dependent,'app_uptime':req.body.app_uptime,'revenue_breach':req.body.revenue_breach,'incidents_month':req.body.incidents_month,'no_ops_troubleshoot':req.body.no_ops_troubleshoot,'no_dev_troubleshoot':req.body.no_dev_troubleshoot,'mttr':req.body.mttr,'no_apps_e2e':req.body.no_apps_e2e,'no_t1t2_apps':req.body.no_t1t2_apps, 'no_fte_existing':req.body.no_fte_existing, 'cycles_per_year':req.body.cycles_per_year, 'cycle_days':req.body.cycle_days, 'test_per_cycle':req.body.test_per_cycle, 'qa_time_per_cycle':req.body.qa_time_per_cycle, 'qa_people_per_cycle': req.body.qa_people_per_cycle, 'dev_time_per_cycle':req.body.dev_time_per_cycle, 'dev_people_per_cycle':req.body.dev_people_per_cycle, 'operation_cost':req.body.operation_cost, 'developer_cost':req.body.developer_cost, 'qa_cost':req.body.qa_cost, 'work_hours':req.body.work_hours, 'benefit_incident_reduction':req.body.benefit_incident_reduction, 'benefit_mttr':req.body.benefit_mttr, 'benefit_performance':req.body.benefit_performance, 'benefit_alert_storm':req.body.benefit_alert_storm, 'benefit_sla':req.body.benefit_sla, 'benefit_fix_qa':req.body.benefit_fix_qa, 'benefit_prod_reduction': req.body.benefit_prod_reduction, 'benefit_config': req.body.benefit_config}});			
+
 				res.writeHead(200, {'Access-Control-Allow-Headers':'content-type'});
 				res.end("success");
 				db.close();					
 			}
-		});	
+		});			
 	});		
-	
+
+	app.get('/getAssessmentData', (req, res) => {	
+
+		var username = req.session.username;
+		MongoClient.connect(connectionOptions, function(err, db) {
+
+			if(err) { 
+				return console.dir(err); 
+				res.writeHead(500, {'Access-Control-Allow-Headers':'content-type'});
+				res.end("failure");
+				db.close();				
+			}	
+			
+			var collection = db.collection('assessment_data');
+			var results = collection.find({'_id':req.header('bva_id')}).toArray(function(err, items) {
+				res.writeHead(200, {'Access-Control-Allow-Headers':'content-type'});
+				res.end(JSON.stringify(items[0]));
+				db.close();
+			})		
+		})	
+	});		
 
 };
 
--- static/bva.js
+++ static/bva.js
@@ -11,39 +11,36 @@ function getNumbers(txt) {
 	}
 }
 
+function getNumbersAndDots(txt) {
+	if (txt != "") {
+		var numb = txt.match(/\d+\.?\d+/g);
+		numb = numb.join("");	
+		return numb;
+	}
+	
+	else {
+		return "";
+	}
+}
+
+function processMoney(preMoney) {
+	if(preMoney != '' && preMoney != undefined) {preMoney = '£' + parseInt(getNumbers(preMoney)).toLocaleString(); temp = preMoney; preMoney = ''; preMoney = temp; return preMoney} else {preMoney = ''; return preMoney}
+}
+
+function processPercent(prePercent) {
+	if(prePercent != '' && prePercent != undefined) {prePercent = getNumbersAndDots(prePercent) + '%'; temp = prePercent; prePercent = ''; prePercent = temp; return prePercent;} else {prePercent = ''; return prePercent;}
+}
+
+function processNumber(preNumber){
+	if(preNumber != '' && preNumber != undefined) {preNumber = parseInt(getNumbers(preNumber)).toLocaleString(); temp = preNumber; preNumber = ''; preNumber = temp; return preNumber; } else {preNumber = ''; return preNumber;}
+}
+
 function failedLogin() {
 	var urlParams = new URLSearchParams(window.location.search);
 	fail = urlParams.get('login');
 	if(fail == "failed") {
 		$(".login-failure").css("display", "block");
 	}
-}
-
-function makeFetch(reqUrl, reqBody, reqMethod, cb) {
-	var myHeaders = new Headers();
-	myHeaders.append("Content-Type", "application/json");
-	myHeaders.append("Accept", "application/json");
-	
-	if(reqMethod == "POST") {
-		var myInit = { method: reqMethod,
-			headers: myHeaders,
-			cache: 'default',
-			body: JSON.stringify(reqBody)
-		}
-	}
-	
-	if(reqMethod == "GET") {
-		var myInit = { method: reqMethod,
-			headers: myHeaders,
-			cache: 'default',
-		}		
-	}
-	
-	fetch(reqUrl, myInit)
-		
-	.then(function(response) {	
-			return response.json;
-	})		
 }
 
 function getAssessmentList() {
@@ -75,12 +72,70 @@ function getAssessmentList() {
 	})
 }
 
+function getAssessmentData() {
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+	myHeaders.append("Accept", "application/json");
+	myHeaders.append("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
+
+	var urlParams = new URLSearchParams(window.location.search);
+	bva_id = urlParams.get('bva_id');
+	
+	myHeaders.append("bva_id", bva_id);
+	
+	var myInit = { method: 'GET',
+		headers: myHeaders,
+		cache: 'default',
+		credentials: 'same-origin'
+	}		
+	
+	fetch('/getAssessmentData', myInit)
+		
+	.then(function(response) {	
+		return response.json();
+	})
+	
+	.then(function(jsonResponse) {	
+		document.getElementById("company_revenue").value = processMoney(jsonResponse.company_revenue);
+		document.getElementById("projected_growth").value = processPercent(jsonResponse.projected_growth);
+		document.getElementById("revenue_dependent").value = processPercent(jsonResponse.revenue_dependent);
+		document.getElementById("app_uptime").value = processPercent(jsonResponse.app_uptime);
+		document.getElementById("revenue_breach").value = processMoney(jsonResponse.revenue_breach);
+		document.getElementById("incidents_month").value = processNumber(jsonResponse.incidents_month);
+		document.getElementById("no_ops_troubleshoot").value = processNumber(jsonResponse.no_ops_troubleshoot);
+		document.getElementById("no_dev_troubleshoot").value = processNumber(jsonResponse.no_dev_troubleshoot);
+		document.getElementById("mttr").value = processNumber(jsonResponse.mttr);
+		document.getElementById("no_apps_e2e").value = processNumber(jsonResponse.no_apps_e2e);
+		document.getElementById("no_t1t2_apps").value = processNumber(jsonResponse.no_t1t2_apps);
+		document.getElementById("no_fte_existing").value = processNumber(jsonResponse.no_fte_existing);
+		document.getElementById("cycles_per_year").value = processNumber(jsonResponse.cycles_per_year);
+		document.getElementById("cycle_days").value = processNumber(jsonResponse.cycle_days);
+		document.getElementById("test_per_cycle").value = processPercent(jsonResponse.test_per_cycle);
+		document.getElementById("qa_time_per_cycle").value = processPercent(jsonResponse.qa_time_per_cycle);
+		document.getElementById("qa_people_per_cycle").value = processNumber(jsonResponse.qa_people_per_cycle);
+		document.getElementById("dev_time_per_cycle").value = processPercent(jsonResponse.dev_time_per_cycle);
+		document.getElementById("dev_people_per_cycle").value = processNumber(jsonResponse.dev_people_per_cycle);
+		document.getElementById("operation_cost").value = processMoney(jsonResponse.operation_cost);
+		document.getElementById("developer_cost").value = processMoney(jsonResponse.developer_cost);
+		document.getElementById("qa_cost").value = processMoney(jsonResponse.qa_cost);
+		document.getElementById("work_hours").value = processNumber(jsonResponse.work_hours);
+		document.getElementById("benefit_incident_reduction").value = processPercent(jsonResponse.benefit_incident_reduction);
+		document.getElementById("benefit_mttr").value = processPercent(jsonResponse.benefit_mttr);
+		document.getElementById("benefit_performance").value = processPercent(jsonResponse.benefit_performance);
+		document.getElementById("benefit_alert_storm").value = processPercent(jsonResponse.benefit_alert_storm);
+		document.getElementById("benefit_sla").value = processPercent(jsonResponse.benefit_sla);
+		document.getElementById("benefit_fix_qa").value = processPercent(jsonResponse.benefit_fix_qa);
+		document.getElementById("benefit_prod_reduction").value = processPercent(jsonResponse.benefit_prod_reduction);
+		document.getElementById("benefit_config").value = processPercent(jsonResponse.benefit_config);
+	})
+}	
+
 function updateAssessment() {
 	var assessment_data = {
 		company_revenue: getNumbers(document.getElementById("company_revenue").value),
-		projected_growth: getNumbers(document.getElementById("projected_growth").value),
-		revenue_dependent: getNumbers(document.getElementById("revenue_dependent").value),
-		app_uptime: getNumbers(document.getElementById("app_uptime").value),
+		projected_growth: getNumbersAndDots(document.getElementById("projected_growth").value),
+		revenue_dependent: getNumbersAndDots(document.getElementById("revenue_dependent").value),
+		app_uptime: getNumbersAndDots(document.getElementById("app_uptime").value),
 		revenue_breach: getNumbers(document.getElementById("revenue_breach").value),
 		incidents_month: getNumbers(document.getElementById("incidents_month").value),
 		no_ops_troubleshoot: getNumbers(document.getElementById("no_ops_troubleshoot").value),
@@ -92,24 +147,113 @@ function updateAssessment() {
 		existing_apps: [],
 		cycles_per_year: getNumbers(document.getElementById("cycles_per_year").value),
 		cycle_days: getNumbers(document.getElementById("cycle_days").value),
-		test_per_cycle: getNumbers(document.getElementById("test_per_cycle").value),
-		qa_time_per_cycle: getNumbers(document.getElementById("qa_time_per_cycle").value),
+		test_per_cycle: getNumbersAndDots(document.getElementById("test_per_cycle").value),
+		qa_time_per_cycle: getNumbersAndDots(document.getElementById("qa_time_per_cycle").value),
 		qa_people_per_cycle: getNumbers(document.getElementById("qa_people_per_cycle").value),
-		dev_time_per_cycle: getNumbers(document.getElementById("dev_time_per_cycle").value),
+		dev_time_per_cycle: getNumbersAndDots(document.getElementById("dev_time_per_cycle").value),
 		dev_people_per_cycle: getNumbers(document.getElementById("dev_people_per_cycle").value),
 		operation_cost: getNumbers(document.getElementById("operation_cost").value),
 		developer_cost: getNumbers(document.getElementById("developer_cost").value),
 		qa_cost: getNumbers(document.getElementById("qa_cost").value),
 		work_hours: getNumbers(document.getElementById("work_hours").value),
-		benefit_incident_reduction: getNumbers(document.getElementById("benefit_incident_reduction").value),
-		benefit_mttr: getNumbers(document.getElementById("benefit_mttr").value),
-		benefit_performance: getNumbers(document.getElementById("benefit_performance").value),
-		benefit_alert_storm: getNumbers(document.getElementById("benefit_alert_storm").value),
-		benefit_sla: getNumbers(document.getElementById("benefit_sla").value),
-		benefit_fix_qa: getNumbers(document.getElementById("benefit_fix_qa").value),
-		benefit_prod_reduction: getNumbers(document.getElementById("benefit_prod_reduction").value),
-		benefit_config: getNumbers(document.getElementById("benefit_config").value)
+		benefit_incident_reduction: getNumbersAndDots(document.getElementById("benefit_incident_reduction").value),
+		benefit_mttr: getNumbersAndDots(document.getElementById("benefit_mttr").value),
+		benefit_performance: getNumbersAndDots(document.getElementById("benefit_performance").value),
+		benefit_alert_storm: getNumbersAndDots(document.getElementById("benefit_alert_storm").value),
+		benefit_sla: getNumbersAndDots(document.getElementById("benefit_sla").value),
+		benefit_fix_qa: getNumbersAndDots(document.getElementById("benefit_fix_qa").value),
+		benefit_prod_reduction: getNumbersAndDots(document.getElementById("benefit_prod_reduction").value),
+		benefit_config: getNumbersAndDots(document.getElementById("benefit_config").value)
 	}
 	
 	console.log(assessment_data);
+	
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+	myHeaders.append("Accept", "application/json");
+	myHeaders.append("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
+	
+	var urlParams = new URLSearchParams(window.location.search);
+	bva_id = urlParams.get('bva_id');
+	
+	myHeaders.append("bva_id", bva_id);
+	
+	var myInit = { method: 'POST',
+		headers: myHeaders,
+		cache: 'default',
+		credentials: 'same-origin',
+		body: JSON.stringify(assessment_data)
+	}		
+	
+	fetch('/updateAssessment', myInit)
+		
+	.then(function(response) {
+		console.log("test update");
+	})
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	
+	$(" .opsFromBiz ").click(function() {
+		$( ".biz" ).hide('slide', {direction: 'left'});
+		$(".ops").fadeIn();
+		
+		$(".leftContainer").css("height", $('.ops').css('height')); 
+		$( ".result-biz" ).hide('slide', {direction: 'left'});
+		$(".result-ops").fadeIn();		 	  	  
+	});	
+		
+	$(" .bizFromOps ").click(function() {
+		$( ".ops" ).toggle( "slide" , { direction: "right" });
+		$( ".biz" ).fadeIn();	
+		$(".leftContainer").css("height", $('.biz').css('height'));
+		$( ".result-ops" ).toggle( "slide" , { direction: "right" });
+		$( ".result-biz" ).fadeIn();	
+	});	
+		
+	$(" .devFromOps ").click(function() {
+		$( ".ops" ).toggle( "slide" , { direction: "left" });
+		$( ".dev" ).fadeIn();		
+		$(".leftContainer").css("height", $('.dev').css('height'));
+		$( ".result-ops" ).toggle( "slide" , { direction: "left" });
+		$( ".result-dev" ).fadeIn();			  
+	});	
+		
+	$(" .opsFromDev ").click(function() {
+		$( ".dev" ).toggle( "slide" , { direction: "right" });
+		$( ".ops" ).fadeIn();	
+		$(".leftContainer").css("height", $('.ops').css('height'));
+		$( ".result-dev" ).toggle( "slide" , { direction: "right" });
+		$( ".result-ops" ).fadeIn();			  
+	});	
+
+	$(" .optionsFromDev ").click(function() {
+		$( ".dev" ).toggle( "slide" , { direction: "left" });
+		$( ".options" ).fadeIn();	
+		$(".leftContainer").css("height", $('.options').css('height'));
+		$( ".result-dev" ).toggle( "slide" , { direction: "left" });
+		$( ".result-options" ).fadeIn();			  
+	});	
+
+	$(" .devFromOptions ").click(function() {
+		$( ".options" ).toggle( "slide" , { direction: "right" });
+		$( ".dev" ).fadeIn();		
+		$(".leftContainer").css("height", $('.dev').css('height'));
+		$( ".result-options" ).toggle( "slide" , { direction: "right" });
+		$( ".result-dev" ).fadeIn();			  
+	});	
+		
+	$('#company_revenue, #revenue_breach, #operation_cost, #developer_cost, #qa_cost').on( "blur", function() {
+		if(this.value != '' && this.value != undefined) {this.value = '£' + parseInt(getNumbers(this.value)).toLocaleString(); temp = this.value; this.value = ''; this.value = temp; } else {this.value = '';}
+	} );
+		
+	$('#projected_growth, #revenue_dependent, #app_uptime, #test_per_cycle, #qa_time_per_cycle, #dev_time_per_cycle, #benefit_incident_reduction, #benefit_mttr, #benefit_performance, #benefit_alert_storm, #benefit_sla, #benefit_fix_qa, #benefit_prod_reduction, #benefit_config').on( "blur", function() {
+		if(this.value != '' && this.value != undefined) {this.value = getNumbersAndDots(this.value) + '%'; temp = this.value; this.value = ''; this.value = temp; } else {this.value = '';}
+	} );
+		
+	$('#incidents_month, #no_ops_troubleshoot, #no_dev_troubleshoot, #mttr, #no_apps_e2e, #no_t1t2_apps, #no_fte_existing, #cycles_per_year, #cycle_days, #qa_people_per_cycle, #dev_people_per_cycle, #work_hours').on( "blur", function() {
+		if(this.value != '' && this.value != undefined) {this.value = parseInt(getNumbers(this.value)).toLocaleString(); temp = this.value; this.value = ''; this.value = temp; } else {this.value = '';}
+	});
+
+	getAssessmentData();	
+});	
