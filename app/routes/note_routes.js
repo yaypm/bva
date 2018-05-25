2d9e004ca9ff0c53ecbf0cc2c06208988a67bf4e
yaypm <aliemslie@gmail.com>
2018-05-25 15:00:49 +0100

    Update note_routes.js

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -44,7 +44,7 @@ module.exports = function(app, db) {
 	}	
 
 	function validateEmail(mail) {
-		if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+		if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(mail)) {
 			return (true)
 		}
 		
