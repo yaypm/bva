dc71838da1b734ceeaa0af00b9501ef740c25dce
yaypm <aliemslie@gmail.com>
2019-01-04 14:38:27 +0000

    Update note_routes.js

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -832,7 +832,7 @@ module.exports = function(app, db) {
 		var usage_array = [50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70];
 		var usage = usage_array[Math.floor((Math.random() * 20) + 1)];
 		
-		var results = {"iops": iops, "volume_latency": volume_latency,  "usage", usage };
+		var results = {"iops": iops, "volume_latency": volume_latency,  "usage": usage };
 		
 		res.writeHead(200, {'Access-Control-Allow-Headers':'content-type'});
 		res.end(JSON.stringify(results));
